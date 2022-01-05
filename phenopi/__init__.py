2295286db8c8e0eefb0829dc5b894162a20f1f1a
Brian James <brianjames567@gmail.com>
2022-01-05 15:08:27 -0600

    Update __init__.py

--- phenopi/__init__.py
+++ phenopi/__init__.py
@@ -6,14 +6,14 @@ from flask_mail import Mail
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '08ce16b3e9f7a1e90861929d6f9d303a'
+app.config['SECRET_KEY'] = 'secret_key'
 #app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'thisis100percentnotfake@gmail.com'
-app.config['MAIL_PASSWORD'] = 'etxp afzg fpqf exup'
+app.config['MAIL_USERNAME'] = 'admin@email.com'
+app.config['MAIL_PASSWORD'] = 'admin_password'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
