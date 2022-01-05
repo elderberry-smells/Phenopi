7eed1ad83fa82a2308f285a282da3ee9c9eca2d9
Brian James <brianjames567@gmail.com>
2022-01-05 10:34:34 -0600

    Update config.py

--- phenopi/config.py
+++ phenopi/config.py
@@ -3,12 +3,10 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = '08ce16b3e9f7a1e90861929d6f9d303a'
+    SECRET_KEY = 'seceret_key'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'thisis100percentnotfake@gmail.com'
-    MAIL_PASSWORD = 'etxp afzg fpqf exup'
-    #MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    #MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = 'you_admin@email.com'
+    MAIL_PASSWORD = 'password for your email'  # probably want to put these into os.envrions so its not hard coded
